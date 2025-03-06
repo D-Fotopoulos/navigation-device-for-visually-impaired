c0938b92e418d8f4958f944e6934132c42cc1769
dilshodas <157644590+dilshodas@users.noreply.github.com>
2025-03-06 17:22:44 -0500

    Update seniordesign.ino

--- seniordesign.ino
+++ seniordesign.ino
@@ -4,10 +4,12 @@
 // The number of sensors in your system.
 const uint8_t sensorCount = 2;
 uint8_t last_distance[2]={0, 0};
+uint8_t current_distance[2]={0, 0};
 uint8_t elapsedTime=0;
+float speed[2]={0, 0};
 
 // The Arduino pin connected to the XSHUT pin of each sensor.
-const uint8_t xshutPins[sensorCount] = { 2, 3};
+const uint8_t xshutPins[sensorCount] = {2, 3};
 
 VL53L1X sensors[sensorCount];
 
@@ -54,36 +56,78 @@ void setup()
 
 void loop()
 {
-  unsigned long startTime = millis(); // Start time in milliseconds
-  for (uint8_t i = 0; i < sensorCount; i++)
-  {
-    uint8_t current=sensors[i].read();
-    Serial.print(current);
-    if (sensors[i].timeoutOccurred()) { 
-      Serial.print(" TIMEOUT"); 
-      //pinMode(xshutPins[i], OUTPUT);
-      //digitalWrite(xshutPins[i], LOW);
-      //Serial.print(" RESET");
-     } // Reinitialize the sensor if necessary}
-    Serial.print('\t');      // Wait 1 second
-    if(last_distance[i]!=0){
-      float speed=(last_distance[i]-current)/elapsedTime;
-      Serial.print("This is speed: ");
-      Serial.print(speed);
+  delay(2000);
+    unsigned long startTime = millis(); // Start time in milliseconds
+    int minDistance = INT_MAX;  // Store closest distance
+    int selectedSensor = -1;    // Store index of the best sensor
+
+    const float speedThreshold = 1.5;   // Minimum speed to consider
+
+    // First, read all sensor values and compute speed
+    for (uint8_t i = 0; i < sensorCount; i++)
+    {
+      Serial.print("Sensor ");
+        Serial.print(i);
+        Serial.print(" Last_Distance: ");
+        Serial.println(last_distance[i]);
+
+
+        current_distance[i] = sensors[i].read();
+        Serial.print("Sensor ");
+        Serial.print(i);
+        Serial.print(" Distance: ");
+        Serial.print(current_distance[i]);
+
+        if (sensors[i].timeoutOccurred()) {
+            Serial.print(" TIMEOUT");
+        }
+
+        Serial.print("\t");
+
+        // Calculate speed only if there's a previous valid reading
+        if (last_distance[i] != 0) {
+            speed[i] = (last_distance[i]-current_distance[i]) / (float)elapsedTime;
+        } else {
+            speed[i] = 0; // If no previous reading, set speed to zero
+        }
+
+        Serial.print("Sensor ");
+        Serial.print(i);
+        Serial.print(" Speed: ");
+        Serial.println(speed[i]);
+
+        last_distance[i] = current_distance[i]; // Update last distance
+
     }
-    
 
-    last_distance[i]=current;//assign it to the last distance after all calculations
-    
-  }
+    // Find the closest valid sensor (only considering speed threshold)
+    for (uint8_t i = 0; i < sensorCount; i++)
+    {
+        if (speed[i] > speedThreshold) {  // Check speed condition
+            if (current_distance[i] < minDistance) { // Choose closest
+                minDistance = current_distance[i];
+                selectedSensor = i;
+            }
+        }
+    }
 
-  Serial.println();
-  tone(6, 500);
-  unsigned long endTime = millis(); // End time in milliseconds
-  elapsedTime = endTime - startTime;
+    // If a valid sensor is found, output tone
+    if (selectedSensor != -1) {
+        Serial.print("Selected Sensor: ");
+        Serial.println(selectedSensor);
+        digitalWrite(6, HIGH);
+        //tone(6, 500, 10); // Output tone for the selected sensor
+    } else {
+        Serial.println("No valid sensor met the speed criteria.");
+    }
 
-  Serial.print("Elapsed time: ");
-  Serial.print(elapsedTime);
-  Serial.println(" ms");
+    unsigned long endTime = millis(); // End time in milliseconds
+    elapsedTime = endTime - startTime;
 
+    Serial.print("Elapsed time: ");
+    Serial.print(elapsedTime);
+    Serial.println(" ms");
+    digitalWrite(6, LOW);
 }
+//learn to use other clocks
+
